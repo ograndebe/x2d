c4cc9de0846b0c0ab7cf4583c5ef04746801500a
rafael <rafael@solutiontrue.com.br>
2018-03-16 14:31:40 -0300

    Implementation of DataRetriever

--- src/main/java/com/github/ograndebe/x2d/DataRetriever.java
+++ src/main/java/com/github/ograndebe/x2d/DataRetriever.java
@@ -1,6 +1,13 @@
 package com.github.ograndebe.x2d;
 
-import org.apache.poi.ss.usermodel.Workbook;
+import com.github.ograndebe.x2d.metadata.Column;
+import com.github.ograndebe.x2d.metadata.Table;
+import org.apache.commons.codec.binary.StringUtils;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.util.StringUtil;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class DataRetriever {
 
@@ -11,6 +18,80 @@ public class DataRetriever {
     }
 
 
+    public List<List<Object>> retrieve(String tableName) {
+        if (tableName == null) throw new IllegalArgumentException("Table name must not be null");
 
+        final int numberOfSheets = workbook.getNumberOfSheets();
+
+        for (int i = 0; i < numberOfSheets; i++) {
+            final Sheet sheet = workbook.getSheetAt(i);
+            if (tableName.equals(tableName(sheet))) {
+                LinkedList<List<Object>> rowsToReturn = new LinkedList<>();
+                for (int j = sheet.getFirstRowNum()+1; j <= sheet.getLastRowNum(); j++) {
+                    final Row row = sheet.getRow(j);
+                    LinkedList<Object> list = new LinkedList<>();
+
+                    for (int z = row.getFirstCellNum(); z <= row.getLastCellNum(); z++) {
+                        final Cell cell = row.getCell(z);
+                        list.add(getValue(cell));
+                    }
+
+                    rowsToReturn.add(list);
+                }
+                return rowsToReturn;
+            }
+        }
+        throw new IllegalArgumentException("There is no table named: "+tableName);
+    }
+
+    private Object getValue(Cell cell) {
+        if (cell == null) return "";
+        Object value;
+        final CellType cellType = cell.getCellTypeEnum();
+        if (cellType == null) return "";
+        switch (cellType) {
+            case STRING:
+                value = cell.getStringCellValue();
+                break;
+            case NUMERIC:
+                value = cell.getNumericCellValue();
+                break;
+            case BOOLEAN:
+                final boolean booleanCellValue = cell.getBooleanCellValue();
+                value = booleanCellValue?"true":"false";
+                break;
+            case BLANK:
+                value = "";
+                break;
+            case ERROR:
+                value = "error";
+                break;
+            case FORMULA:
+                try {
+                    value = cell.getStringCellValue();
+                } catch (Exception e) {
+                    try {
+                        value = cell.getNumericCellValue();
+                    } catch (Exception e2) {
+                        value = "error";
+                    }
+                }
+                break;
+            case _NONE:
+                value = "error";
+                break;
+            default:
+                value = "";
+                break;
+
+        }
+
+        return value;
+    }
+
+    private String tableName(final Sheet sheet) {
+        if (sheet == null) throw new IllegalArgumentException("Sheet must be not null");
+        return sheet.getSheetName().replace(" ","_").toUpperCase();
+    }
 
 }
---
+++ src/test/java/com/github/ograndebe/x2d/DataRetrieverTest.java
@@ -0,0 +1,48 @@
+package com.github.ograndebe.x2d;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class DataRetrieverTest {
+
+    @Test
+    public void retrieve() throws IOException, InvalidFormatException {
+        final InputStream resourceAsStream = this.getClass().getResourceAsStream("/sample_sheet.xls");
+        Workbook wb = WorkbookFactory.create(resourceAsStream);
+        DataRetriever dr = new DataRetriever(wb);
+
+        List<List<Object>> data = dr.retrieve("PEOPLE");
+
+        assertNotNull(data);
+        assertEquals(4,data.size());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testIllegalArgument() throws IOException, InvalidFormatException {
+        final InputStream resourceAsStream = this.getClass().getResourceAsStream("/sample_sheet.xls");
+        Workbook wb = WorkbookFactory.create(resourceAsStream);
+        DataRetriever dr = new DataRetriever(wb);
+
+        dr.retrieve("NAME_THAT_NOT_EXISTS");
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNull() throws IOException, InvalidFormatException {
+        final InputStream resourceAsStream = this.getClass().getResourceAsStream("/sample_sheet.xls");
+        Workbook wb = WorkbookFactory.create(resourceAsStream);
+        DataRetriever dr = new DataRetriever(wb);
+
+        dr.retrieve(null);
+    }
+
+}
